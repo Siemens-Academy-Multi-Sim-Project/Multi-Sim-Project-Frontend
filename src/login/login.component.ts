719d47b7e069e605277f807c6ca5145b09114b1b
sara-montasser <saramontasser4@gmail.com>
2023-04-18 06:15:54 +0200

    feat: create login page

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import {faUser} from '@fortawesome/free-solid-svg-icons'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  faUser = faUser;
 }
