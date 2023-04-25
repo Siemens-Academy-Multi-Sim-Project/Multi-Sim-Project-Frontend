7f7e08efdf3c7e6585349354433822ec65a2c27a
sara-montasser <saramontasser4@gmail.com>
2023-04-25 03:00:04 +0200

    feat: adding first and last name validations

--- src/register/register.component.ts
+++ src/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {faUser} from '@fortawesome/free-solid-svg-icons'
+import {FormBuilder,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,4 +9,13 @@ import {faUser} from '@fortawesome/free-solid-svg-icons'
 })
 export class RegisterComponent {
   faUser = faUser;
+  constructor(private fb:FormBuilder){}
+  registerationForm = this.fb.group({
+    firstName:['',[Validators.required,Validators.pattern('[a-zA-Z ]*')]],
+    lastName: ['',[Validators.required,Validators.pattern('[a-zA-Z ]*')]],
+    email: ['',Validators.required],
+    password: ['',Validators.required],
+    confirmPassword: ['',Validators.required]
+  });
+  
 }
