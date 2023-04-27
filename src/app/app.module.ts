ced134d1dc48a49d24b23bc28472e3cbfb4f5b42
Omar Atef <20190356@stud.fci-cu.edu.eg>
2023-04-27 22:23:01 +0200

    Session Overview Component Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import {AppRoutingModule, routingComponents} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { SessionOverviewComponent } from './components/session-overview/session-overview.component';
+import { SingleAttributeComponent } from './components/session-overview/single-attribute/single-attribute.component';
+import { MultiAttributeComponent } from './components/session-overview/multi-attribute/multi-attribute.component';
+import { DualAttributeComponent } from './components/session-overview/dual-attribute/dual-attribute.component';
 
 
 @NgModule({
@@ -13,6 +16,9 @@ import { SessionOverviewComponent } from './components/session-overview/session-
     AppComponent,
     routingComponents,
     SessionOverviewComponent,
+    SingleAttributeComponent,
+    MultiAttributeComponent,
+    DualAttributeComponent,
   ],
   imports: [
     BrowserModule,
