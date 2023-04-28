c83757d3de3f0190e163d2f47382036009cde514
Peter Atef <peter.atef@coformatique.com>
2023-04-28 21:28:57 +0200

    first commit

---
+++ src/app/overview-bar-chart/overview-bar-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overview-bar-chart',
+  templateUrl: './overview-bar-chart.component.html',
+  styleUrls: ['./overview-bar-chart.component.css']
+})
+export class OverviewBarChartComponent {
+
+}
