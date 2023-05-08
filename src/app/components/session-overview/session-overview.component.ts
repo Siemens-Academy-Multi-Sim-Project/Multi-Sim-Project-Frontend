5c22721947fcf56db97022ad2824e693be74e561
Omar Atef <20190356@stud.fci-cu.edu.eg>
2023-05-08 22:10:54 +0200

    Completed Usage Profiling Table services (still can improve more)

--- src/app/components/session-overview/session-overview.component.ts
+++ src/app/components/session-overview/session-overview.component.ts
@@ -5,6 +5,8 @@ import {DualAttribute} from "../../models/session-overview-models/dual-attribute
 import {UsageProfileTableComponent} from "../usage-profile-table/usage-profile-table.component";
 import {OverviewService} from 'src/app/services/overview-service/overview.service';
 import {ActivatedRoute} from '@angular/router';
+import {Columns} from "../../models/usage-profile/columns";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-session-overview',
@@ -17,13 +19,15 @@ export class SessionOverviewComponent {
     console.log(this.route.snapshot.queryParamMap.get('clusterId'));
 
 
-    service.getClusterById(12).subscribe((data) => {
+    service.getClusterById(6).subscribe((data) => {
       console.log(data);
       this.service.set_profiling_data(data);
       console.log(this.service.profilingDataArray);
       this.singleAttributes = [this.service.getTotalSimulations(), this.service.getDesigns()]
       this.multiAttributes = [this.service.getVoptTime_multiAttr(), this.service.getVsimTime_multiAttr(), this.service.getVoptMemory_multiAttr(), this.service.getVsimMemory_multiAttr()]
       this.dualAttribute = this.service.getSamplesAndCalls();
+      this.usageProfileData = this.service.getUsageProfilingData();
+      this.table_data = new MatTableDataSource<Columns>(this.usageProfileData);
     })
   }
 
@@ -31,5 +35,7 @@ export class SessionOverviewComponent {
   singleAttributes: SingleAttribute[] = [];
   multiAttributes: MultiAttribute[] = [];
   dualAttribute: DualAttribute = {calls: 0, samples: 0};
+  usageProfileData: Columns[] = [];
+  table_data: MatTableDataSource<Columns> = new MatTableDataSource<Columns>(this.usageProfileData);
 
 }
