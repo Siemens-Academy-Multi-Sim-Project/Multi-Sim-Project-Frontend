54f9fde4322edc27bfaebbfe03c03e57a8986fa6
Peter Atef <peter.atef@coformatique.com>
2023-05-09 10:40:03 +0200

    added coloring based on value

--- src/app/models/session-overview-models/bar-chart-models/ChartOptions.ts
+++ src/app/models/session-overview-models/bar-chart-models/ChartOptions.ts
@@ -6,7 +6,8 @@ import {
     ApexTitleSubtitle,
     ApexDataLabels,
     ApexPlotOptions,
-    ApexLegend
+    ApexLegend,
+    ApexTooltip
 } from "ng-apexcharts";
 
 export class BarChartOptions {
@@ -46,23 +47,22 @@ export class TreeMapChartOptions {
     chart!: ApexChart;
     dataLabels!: ApexDataLabels;
     title!: ApexTitleSubtitle;
-    plotOptions!: ApexPlotOptions;
     legend!: ApexLegend;
 
     static createTreeMapChartOptions(localHits: Map<string, number>, avgPercentages: Map<string, number>): TreeMapChartOptions {
-        let dataSeries: {x:string, y: number, fillColor: string}[] = []
+        let dataSeries: { x: string, y: number, fillColor: string }[] = []
 
         localHits.forEach((value, key) => {
-            dataSeries.push({ 
-                x: key, 
-                y:value,
+            dataSeries.push({
+                x: key,
+                y: value,
                 fillColor: mapNumbersToColors(avgPercentages.get(key))
             })
         })
 
         return {
             series: [
-                {data: dataSeries}
+                { data: dataSeries }
             ],
             legend: {
                 show: false,
@@ -78,28 +78,7 @@ export class TreeMapChartOptions {
             },
             dataLabels: {
                 enabled: true,
-                offsetY: -3
-            },
-            plotOptions: {
-                treemap: {
-                    enableShades: true,
-                    shadeIntensity: 0.5,
-                    reverseNegativeShade: true,
-                    colorScale: {
-                        ranges: [
-                            {
-                                from: -6,
-                                to: 0,
-                                color: "#CD363A"
-                            },
-                            {
-                                from: 0.001,
-                                to: 6,
-                                color: "#52B12C"
-                            }
-                        ]
-                    }
-                }
+                offsetY: -3,
             }
         };
     }
@@ -108,16 +87,16 @@ export class TreeMapChartOptions {
 function mapNumbersToColors(num: number | undefined): string {
     const [startNum, endNum] = [0, 1];
     const [startColor, endColor] = ["#FF0000", "#00FF00"];
-    
-    if(num == null) return startColor
+
+    if (num == null) return startColor
 
     const colorMap = (n: number): string => {
-      const p = (n - startNum) / (endNum - startNum); // Map current number value to 0-1 range
-      const r = Math.round((1 - p) * parseInt(startColor.substring(1, 3), 16) + p * parseInt(endColor.substring(1, 3), 16)).toString(16).padStart(2, "0"); // Calculate red value of resulting color
-      const g = Math.round((1 - p) * parseInt(startColor.substring(3, 5), 16) + p * parseInt(endColor.substring(3, 5), 16)).toString(16).padStart(2, "0"); // Calculate green value of resulting color
-      const b = Math.round((1 - p) * parseInt(startColor.substring(5, 7), 16) + p * parseInt(endColor.substring(5, 7), 16)).toString(16).padStart(2, "0"); // Calculate blue value of resulting color
-      return `#${r}${g}${b}`; // Return resulting color
+        const p = (n - startNum) / (endNum - startNum); // Map current number value to 0-1 range
+        const r = Math.round((1 - p) * parseInt(startColor.substring(1, 3), 16) + p * parseInt(endColor.substring(1, 3), 16)).toString(16).padStart(2, "0"); // Calculate red value of resulting color
+        const g = Math.round((1 - p) * parseInt(startColor.substring(3, 5), 16) + p * parseInt(endColor.substring(3, 5), 16)).toString(16).padStart(2, "0"); // Calculate green value of resulting color
+        const b = Math.round((1 - p) * parseInt(startColor.substring(5, 7), 16) + p * parseInt(endColor.substring(5, 7), 16)).toString(16).padStart(2, "0"); // Calculate blue value of resulting color
+        return `#${r}${g}${b}`; // Return resulting color
     }
-  
+
     return colorMap(num) // Map numbers to colors
-  }
+}
